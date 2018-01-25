823bad460482802d57e0cfe67c7fa80cdd8d0c55
upretymadan45 <upretymadan45@gmail.com>
2018-01-25 13:33:06 +1300

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,56 +1,57 @@
 $((function(){
-    var url;
-    var redirectUrl;
+            var url;
+            var redirectUrl;
+            var target;
 
-    $('body').append(`
-            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Warning</h4>
-                </div>
-                <div class="modal-body delete-modal-body">
-                    
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal" id="cancel-delete">Cancel</button>
-                    <button type="button" class="btn btn-danger" id="confirm-delete">Delete</button>
-                </div>
-                </div>
-            </div>
-            </div>`);
+            $('body').append(`
+                    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="myModalLabel">Warning</h4>
+                        </div>
+                        <div class="modal-body delete-modal-body">
+                            
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal" id="cancel-delete">Cancel</button>
+                            <button type="button" class="btn btn-danger" id="confirm-delete">Delete</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>`);
 
-     //Delete Action
-     $(".delete").on('click',(e)=>{
-         e.preventDefault();
+            //Delete Action
+            $(".delete").on('click',(e)=>{
+                e.preventDefault();
 
-        var target = e.target;
-        var Id = $(target).attr('data-Id');
-        var controller = $(target).attr('data-controller');
-        var action = $(target).attr('data-action');
-        var bodyMessage = $(target).attr('data-body-message');
-        redirectUrl = $(target).attr('data-redirect-url');
+                target = e.target;
+                var Id = $(target).data('id');
+                var controller = $(target).data('controller');
+                var action = $(target).data('action');
+                var bodyMessage = $(target).data('body-message');
+                redirectUrl = $(target).data('redirect-url');
 
-        url = "/"+controller+"/"+action+"?Id="+Id;
-        $(".delete-modal-body").text(bodyMessage);
-        $("#deleteModal").modal('show');
-    });
-
-    $("#confirm-delete").on('click',()=>{
-        $.get(url)
-            .done((result)=>{
-                window.location.href = redirectUrl;                    
-            })
-            .fail((error)=>{              
-                window.location.href = redirectUrl;
-            }).always(()=>{
-                $("#deleteModal").modal('hide');                    
+                url = "/"+controller+"/"+action+"?Id="+Id;
+                $(".delete-modal-body").text(bodyMessage);
+                $("#deleteModal").modal('show');
             });
-    });
 
-    $("#deleteModal").on('hidden.bs.modal', function (e) {
-        e.preventDefault();
-    });
+            $("#confirm-delete").on('click',()=>{
+                $.get(url)
+                    .done((result)=>{
+                        if(!redirectUrl){
+                            return $(target).parent().parent().hide("slow");
+                        }
+                        window.location.href = redirectUrl;                    
+                    })
+                    .fail((error)=>{ 
+                        if(redirectUrl)             
+                            window.location.href = redirectUrl;
+                    }).always(()=>{
+                        $("#deleteModal").modal('hide');                    
+                    });
+            });
 
-}()));
+        }()));
